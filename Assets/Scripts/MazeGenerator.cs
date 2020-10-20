d1d6c2d3a6fb9d12ce1799248c4e306a57d02259
Emilia <42769841+emiliaszynkowska@users.noreply.github.com>
2020-10-20 14:46:03 +0100

    Implementing Corridors - Attempt 1
    - makes weird blocks coming out of the rooms. fix

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -30,7 +30,7 @@ public class MazeGenerator : MonoBehaviour
     public int maxRooms;
     public int minRoomSize;
     public int maxRoomSize;
-    private ArrayList rooms;
+    private Room[] rooms;
     
     // Start is called before the first frame update
     void Start()
@@ -41,7 +41,8 @@ public class MazeGenerator : MonoBehaviour
     // Update is called once per frame
     void PlaceRooms()
     {
-        rooms = new ArrayList();
+        rooms = new Room[]{};
+        (int, int) newCenter;
         Random random = new Random();
 
         for (int i = 0; i < maxRooms; i++)
@@ -63,15 +64,46 @@ public class MazeGenerator : MonoBehaviour
             if (success)
             {
                 CreateRoom(newRoom);
-                rooms.Add(newRoom);
+                newCenter = newRoom.c;
+
+                if (rooms.Length != 0)
+                {
+                    (int, int) prevCenter = rooms[rooms.Length - 1].c;
+                    if (random.Next(2) == 1)
+                    {
+                        HorizontalCorridor(prevCenter.Item1, newCenter.Item1, prevCenter.Item2);
+                        VerticalCorridor(prevCenter.Item2, newCenter.Item2, newCenter.Item1);
+                    }
+                    else
+                    {
+                        VerticalCorridor(prevCenter.Item2, newCenter.Item2, prevCenter.Item1);
+                        HorizontalCorridor(prevCenter.Item1,newCenter.Item1,newCenter.Item2);
+                    }
+                }
+                
+                rooms.Append(newRoom);
             }
         }
     }
 
     void CreateRoom(Room room)
     {
-        groundMap.SetTile(new Vector3Int(room.centerX,room.centerY,0), groundTile);
+        groundMap.SetTile(new Vector3Int(room.c.Item1,room.c.Item2,0), groundTile);
         groundMap.BoxFill(new Vector3Int(room.x1,room.y1,0), groundTile, room.x1, room.y1, room.x2, room.y2);
+    }
+
+    void VerticalCorridor(int y1, int y2, int x)
+    {
+        Random random = new Random();
+        foreach (var y in Enumerable.Range(Math.Min(y1, y2), Math.Max(y1, y2)))
+            groundMap.SetTile(new Vector3Int(x,y,0), groundTile);
+    }
+    
+    void HorizontalCorridor(int x1, int x2, int y)
+    {
+        Random random = new Random();
+        foreach (var x in Enumerable.Range(Math.Min(x1, x2), Math.Max(x1, x2)))
+            groundMap.SetTile(new Vector3Int(x,y,0), groundTile);
     }
 
 }
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,18 +1,17 @@
 using System;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+public class Room 
 {
     // Coordinates
     public int x1;
     public int x2;
     public int y1;
     public int y2;
-    public int centerX;
-    public int centerY;
     // Dimensions
     public int w;
     public int h;
+    public (int, int) c;
 
     public Room(int x, int y, int w, int h)
     {
@@ -22,13 +21,12 @@ public class Room : MonoBehaviour
         y2 = y + h;
         this.w = w;
         this.h = h;
-        centerX = (int)Math.Round((double)(x1 + x2) / 2);
-        centerY = (int)Math.Round((double)(y1 + y2) / 2);
+        c = ((int) Math.Round((double) (x1 + x2) / 2),(int) Math.Round((double) (y1 + y2) / 2));
     }
-    
+
     public bool Intersects(Room room)
     {
         return (x1 <= room.x2 && x2 >= room.x1 && y1 <= room.y2 && room.y2 >= room.y1);
     }
-    
+
 }
